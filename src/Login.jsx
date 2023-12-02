0f3bba0fd2b10031d028057a111e0948054556a6
MrZ2001 <kavinshachamod2001@gmail.com>
2023-12-02 14:52:01 +0530

    First Commit

---
+++ src/Login.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [values,setValues] = useState({
+
+    email:'',
+    password:''
+  })
+  const navigate = useNavigate('/')
+
+  axios.defaults.withCredentials = true;
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    axios.post('http://localhost:8081/login',values)
+    .then(res => {
+      if(res.data.Status === "Success"){
+        navigate('/')
+      }
+      else{
+        alert(res.data.Message)
+      }
+    })
+    .catch(err => console.log(err));
+
+  }
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-50">
+        <h2>Sign In</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              name="email"
+              autoComplete="off"
+              onChange={e =>setValues({...values, email: e.target.value})}
+              className="form-control rounded-5"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              onChange={e =>setValues({...values, password: e.target.value})}
+              className="form-control rounded-5"
+            />
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-5">
+            Login
+          </button>
+          <p>You agree to our Terms and Policies</p>
+          <button className="btn btn-default border w-100 bg-light rounded-5 text-decoration-none">Create Account</button>
+        </form>
+      </div>
+    </div>
+  );
+}
